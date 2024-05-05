7d87a4c20e1e6af1ba7645247f53b60b27f1bb5d
narsis <narsiskh.2013@gmail.com>
2024-05-05 14:07:48 +0330

    The first Node program

---
+++ app.js
@@ -0,0 +1,7 @@
+function sayHello(name) {
+    console.log('Hello ' + name);
+}
+
+// sayHello('Narcis')
+console.log(window); // Error: window is not defined // In Node we don`t have the window or document objects
+// In Node we have other objects to work with files with the operating system with the network and so on.
