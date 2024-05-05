ff4273e04b774200d514782d7f92a7c9d22852dd
narsis <narsiskh.2013@gmail.com>
2024-05-05 14:21:04 +0330

    Learning about global objects.

---
+++ GlobalObject.js
@@ -0,0 +1,5 @@
+var messages = '';
+// window.message
+//In Node
+// global.setInterval()
+console.log(global.messages); // undefined
---
+++ main.js
@@ -0,0 +1,14 @@
+/* Global Objects In JavaScript */
+//1
+console.log(); // Global
+
+//2
+setTimeout() // We use this to call a function after a delay like one second or whatever
+//3
+clearTimeout()
+
+//4
+setInterval() // We use this to repeatedly call a function after a given delay
+//5
+clearInterval() // We use this to stop that function from being called repeatedly
+
