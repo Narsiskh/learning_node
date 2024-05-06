9751b3a2adeaea812d09cc041a2f076c88fed421
narsis <narsiskh.2013@gmail.com>
2024-05-06 14:21:08 +0330

    Learning about modules.

--- modules.js
+++ modules.js
@@ -1,2 +1,17 @@
 /* Here is our main module */
 console.log(module);
+// Module {
+//   id: '.',
+//   path: '/home/narsis/Project/learning_node',
+//   exports: {},
+//   filename: '/home/narsis/Project/learning_node/modules.js',
+//   loaded: false,
+//   children: [],
+//   paths: [
+//     '/home/narsis/Project/learning_node/node_modules',
+//     '/home/narsis/Project/node_modules',
+//     '/home/narsis/node_modules',
+//     '/home/node_modules',
+//     '/node_modules'
+//   ]
+// }
