92346509318d4ca5b38a67c98822994730916ddf
narsis <narsiskh.2013@gmail.com>
2024-05-11 20:37:12 +0330

    Learning about extending EventEmitter

--- event.js
+++ event.js
@@ -2,7 +2,19 @@
 // The first letter of every word is uppercase. This is the convention that indicates that this event emitter is a class.
 const EventEmitter = require('events')
 // Here this emitter is an object
-const emitter = new EventEmitter()
+//const emitter = new EventEmitter()
+
+/* Raise an event */
+// We use `emit` method to raise an event.
+// Emit basically means making a noise, or produce something - You are signaling that an event has happened
+// The second argument is called event argument
+// emitter.emit('messageLogged', {id: 1, url: 'https://'})
+
+/* Raise: logging (data: message) */
+// emitter.emit('messageLogged', {id: 2, url: 'https://'})
+
+const Logger = require('./logger')
+const logger = new Logger()
 
 /* Register a listener */
 // A listener is a function that will be called when that event is raised
@@ -10,14 +22,9 @@ const emitter = new EventEmitter()
 // `on` method takes two arguments: 1) Name of the event  2) A callback function or Listener
 // The listener can also receive the event argument which you can call it anything such as arg, e, or eventArg
 // We can use arrow function instead of the function keyword
-emitter.on('messageLogged', (arg) => {
-    console.log('Listener called', arg) // Listener called  { id: 1, url: 'https://' }
-})
-/* Raise an event */
-// We use `emit` method to raise an event.
-// Emit basically means making a noise, or produce something - You are signaling that an event has happened
-// The second argument is called event argument
-emitter.emit('messageLogged', {id: 1, url: 'https://'})
+logger.on('messageLogged', (arg) => {
+    console.log('Listener called', arg) // message // Listener called { id: 1, url: 'https://' }
 
-/* Raise: logging (data: message) */
-emitter.emit('messageLogged', {id: 2, url: 'https://'})
+})
+
+logger.log('message') // message // So, the event listener was not called because here we are working with two different app emitters
--- logger.js
+++ logger.js
@@ -1,19 +1,32 @@
 // (function (exports, require, module, __filename, __dirname) {})
 
-console.log(__filename); // /home/narsis/Project/learning_node/logger.js
-console.log(__dirname); // /home/narsis/Project/learning_node
+// console.log(__filename); // /home/narsis/Project/learning_node/logger.js
+// console.log(__dirname); // /home/narsis/Project/learning_node
+
+const EventEmitter = require('events')
+
 // let x =; // SyntaxError: Unexpected token ';'
 let url = 'https://mylogger.io/log';
 
-/* Exporting Modules */
+// We want this Logger class to have all the capabilities of this event emitter. The way we dp that is by using the `extends` keyword. Then we add the name of parent or the base class. With this simple change this Logger class will have all the functionality that is defined in EventEmitter.
+class Logger extends EventEmitter {
+    /* Exporting Modules */
 // To make functions, objects, or variables available for use in other files, you need to explicitly export them from the module file. This is typically done using the module.exports or exports object. For example:
-function log(message) {
-    // Send an HTTP request
-    console.log(message);
+// When a function is inside a class, we say that is a method in that class
+    log(message) {
+        // Send an HTTP request
+        console.log(message);
+
+        /* Raise an event */
+        //emitter.emit('messageLogged', {id: 1, url: 'https://'})
+        // Now instead of using this `emitter` object, we are going to use `this`. So, in this class we can directly emit or raise events
+        this.emit('messageLogged', {id: 1, url: 'https://'})
+    }
+
 }
 
 
-module.exports = log  // We want export just a single function. So the `logger` in modules.js is no longer an object, it is a function that we can call directly
+module.exports = Logger  // We want export just a single function. So the `logger` in modules.js is no longer an object, it is a function that we can call directly
 
 // module.exports.log = log
 // exports.log = log;
